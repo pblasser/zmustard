fbbc93c0b84820ed47c2d0599227f4ef23370f83
Peter Blasser <34670846+pblasser@users.noreply.github.com>
2026-03-22 17:44:33 +0100

    Update hedr.lua

--- hedr.lua
+++ hedr.lua
@@ -4,7 +4,21 @@ feed=1500
 fart=1/10
 high=0
 
-t
+Element={dx=0,dy=0}
+function Element.retact()
+extrude narg
+ print("G0z%d\n",high+2
+end
+function Element.puddon()
+ extrude zerp
+p g1 + high slow
+end
+function Element.skimto(x,y)
+ retract
+ print(x+dx 
+ puddon
+end
+
 
 eaves
 spires
